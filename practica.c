781f4501a5530338ff19dfe2fb587e9f00034341
LOVERWIN <erwin.delgado79@unach.mx>
2023-06-07 21:15:36 -0600

    Se actualizo el archivo c ahora pide un numero

---
+++ practica.c
@@ -0,0 +1,14 @@
+#inclue<stdlib>
+
+main(){
+int a;
+printf("hello world");
+printf("Aqui modifico el archivo");
+printf("Ingrese un numero entre 1 y 2")
+scanf("%d",&a);
+if(a == 1){
+    printf("El numero que introducido es 1");
+}else{
+    printf("el numero introducido es 2");
+}
+}
