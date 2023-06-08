8ea87eb5bcdf2135ca0be42ad8517a7d07a3cdfd
LOVERWIN <erwin.delgado79@unach.mx>
2023-06-08 00:14:53 -0600

    seria el 4 commit

---
+++ db.sql
@@ -0,0 +1,8 @@
+SELECT * FROM clientes;
+
+CREATE TABLE clientes(
+    id_cliente INT PRIMARY KEY,
+    nombre VARCHAR,
+    ap_paterno VARCHAR,
+    ap_materno VARCHAR
+);
--- practica.c
+++ practica.c
@@ -8,7 +8,9 @@ printf("Ingrese un numero entre 1 y 2")
 scanf("%d",&a);
 if(a == 1){
     printf("El numero que introducido es 1");
-}else{
+}else if(a == 2){
     printf("el numero introducido es 2");
+}else{
+    printf("numero ingresado no esta comprendido entre el 1y el 2")
 }
 }
