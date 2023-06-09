66b001663df5a2cd3f3bdae147ee7d08156660d3
LOVERWIN <erwin.delgado79@unach.mx>
2023-06-09 00:31:41 -0600

    repositorio github

--- db.sql
+++ db.sql
@@ -4,5 +4,8 @@ CREATE TABLE clientes(
     id_cliente INT PRIMARY KEY,
     nombre VARCHAR,
     ap_paterno VARCHAR,
-    ap_materno VARCHAR
+    ap_materno VARCHAR 
 );
+
+-- ahora subiremos esta modificacion a nuestro
+--repositorio en github
